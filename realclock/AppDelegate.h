48cba4e8dd82a2b82b01c5647d11077c7d46262d
Alessandro Giacomella <alessandro.giacomella@gmail.com>
2015-11-02 13:36:27 +0100

    Initial Commit

---
+++ realclock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  realclock
+//
+//  Created by Alessandro Giacomella on 02/11/15.
+//  Copyright © 2015 AleGiac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
