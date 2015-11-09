b58978174132c9682fef062561c7cd5a277b7a26
Alessandro Giacomella <alessandro.giacomella@gmail.com>
2015-11-09 14:48:09 +0100

    First import

--- realclock/ViewController.m
+++ realclock/ViewController.m
@@ -9,19 +9,59 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *hour1Label;
+@property (weak, nonatomic) IBOutlet UILabel *hour2Label;
+@property (weak, nonatomic) IBOutlet UILabel *separator1Label;
+@property (weak, nonatomic) IBOutlet UILabel *minute1Label;
+@property (weak, nonatomic) IBOutlet UILabel *minute2Label;
+@property (weak, nonatomic) IBOutlet UILabel *separator2Label;
+@property (weak, nonatomic) IBOutlet UILabel *second1Label;
+@property (weak, nonatomic) IBOutlet UILabel *second2Label;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    // Shows the time in view
+    [self showTime];
+    
+    // Setup the timer
+    NSRunLoop *runloop = [NSRunLoop currentRunLoop];
+    NSTimer *clockTimer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(showTime) userInfo:nil repeats:YES];
+    [runloop addTimer:clockTimer forMode:NSRunLoopCommonModes];
+    [runloop addTimer:clockTimer forMode:UITrackingRunLoopMode];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+}
+
+-(void)showTime
+{
+    NSDate *date = [NSDate date];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    
+    [formatter setDateFormat:@"hh:mm:ss a"];
+    NSString *time = [formatter stringFromDate:date];
+    
+    _hour1Label.text = [time substringWithRange:NSMakeRange(0,1)];
+    _hour2Label.text = [time substringWithRange:NSMakeRange(1,1)];
+    
+    _minute1Label.text = [time substringWithRange:NSMakeRange(3,1)];
+    _minute2Label.text = [time substringWithRange:NSMakeRange(4,1)];
+    
+    _second1Label.text = [time substringWithRange:NSMakeRange(6,1)];
+    _second2Label.text = [time substringWithRange:NSMakeRange(7,1)];
+    
+    
+    
 }
 
 @end
